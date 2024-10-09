e97b9ba9e78f5baca2e8aff14597707a208981f8
RAhul <iamrahul121@outlook.com>
2024-10-09 09:18:33 +0530

    minor changes

--- frontend/src/components/Pin/Pin.jsx
+++ frontend/src/components/Pin/Pin.jsx
@@ -4,11 +4,12 @@ import { Marker, Popup } from "react-leaflet";
 import { Link } from "react-router-dom";
 
 const Pin = ({ item }) => {
+  // console.log(item);
   return (
     <Marker position={[item.latitude, item.longitude]}>
       <Popup>
         <div className="popupWrapper">
-          <img src={item.img} alt="" />
+          <img src={item.media[0]} alt="" />
           <div className="textWrapper">
             <Link to={`/post/${item.id}`}>{item.title}</Link>
             <span className="bedCount">{item.bedroom} bedroom</span>
