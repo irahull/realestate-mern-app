7c0e798dc54ea49756e12132dafc77ba2ea3e88a
RAhul <iamrahul121@outlook.com>
2024-10-09 08:54:25 +0530

    new

--- frontend/src/helper/apiRequest.js
+++ frontend/src/helper/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:4000/api",
-  // baseURL: "https://realestate-mern-backend.onrender.com/api",
+  // baseURL: "http://localhost:4000/api",
+  baseURL: "https://realestate-mern-backend.onrender.com/api",
   withCredentials: true,
 });
 
