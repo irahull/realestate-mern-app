657d9c876887e27c99bc90de98315455214fc699
RAhul Kumar <131592516+irahull@users.noreply.github.com>
2024-10-09 11:41:47 +0530

    Update apiRequest.js

--- frontend/src/helper/apiRequest.js
+++ frontend/src/helper/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  // baseURL: "http://localhost:4000/api",
-  baseURL: "https://realestate-mern-backend.onrender.com/api",
+  baseURL: "http://localhost:4000/api",
+  // baseURL: "Enter backend Url Upper",
   withCredentials: true,
 });
 
